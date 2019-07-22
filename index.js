dd6c2c0f484880327cd8d54af095959864b2a873
Rabbani <Rabbani.Bhuyain@insight.com>
2019-07-22 14:48:14 -0400

    Updated app

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+
 var os = require("os");
 
 const app = express();
+app.use('/images', express.static(__dirname + "/images"));
 const port = 9000;
 var hostname = os.hostname();
 app.get('/', (req, res) => {
-    html = "<div style='text-align:center;margin:20px;'><img src='https://www.insight.com/content/dam/insight-web/logos/global-nav.svg' width='30%' alt='Insight'> <h1>Hello</h1><h2>Version : 1.0</h2> <h2>Machine Name: "+hostname+"</h2></div>"
+    html = " <div style='text-align:center;margin:20px;'> <img src='./images/kubernetes.png' width='30%' alt='Insight'><h1>Welcome!</h1><h2>Version : 1.0</h2> <h2>Machine: "+hostname+"</h2></div><br/><br/><p></p><div style='text-align:center;margin:20px;'> <img src='./images/lunchgnlearn.png' width='20%' alt='Insight'>"
     res.send(html);
 });
 app.listen(port, () => {
