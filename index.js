bf5c7bc7b211b23a7088cb2f68579e4dcf709dd9
Rabbani <Rabbani.Bhuyain@insight.com>
2019-07-24 12:38:32 -0400

    v2.1 again

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use('/images', express.static(__dirname + "/images"));
 const port = 80;
 var hostname = os.hostname();
-var appVersion= "1.4";
+var appVersion= "2.1";
 app.get('/', (req, res) => {
     html = " <div style='text-align:center;margin:20px;'> <img src='./images/kubernetes.png' width='30%' alt='Insight'><h1>Welcome!</h1><h2>Version : "+appVersion+"</h2> <h2>Machine: "+hostname+"</h2></div><br/><br/><p></p><div style='text-align:center;margin:20px;'> <img src='./images/lunchgnlearn.png' width='20%' alt='Insight'>"
     res.send(html);
